3b823bc43e8dc1a15daadd406229dacbd77349b4
Matthew Wardrop <mister.wardrop@gmail.com>
2014-03-11 07:39:09 +1100

    Bump version number to 1.1.2 .

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
     ]
 
 setup(name='python-parameters',
-      version='1.1.1',
+      version='1.1.2',
       description='A parameter manager that keeps track of physical (or numerical) quantities, and the relationships between them.',
       author='Matthew Wardrop',
       author_email='mister.wardrop@gmail.com',
